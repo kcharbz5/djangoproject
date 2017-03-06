be14a943825d4774719ba7d4b5dfad51821203a7
Kevin Charbonneau <kevin.charbonneau.1@ens.etsmtl.ca>
2017-03-06 11:43:42 -0500

    some additions

--- music/views.py
+++ music/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Album
 
 def index(request):
-    return HttpResponse("<h1>Music page</hi>")
+    all_albums = Album.objects.all()
+    html = '<h1>This is the list of albums</hi>' \
+           '<br></h1>'
+
+    for album in all_albums:
+        url = '/music/' + str(album.id) + '/'
+        html += '<a href="' + url + '">' + album.album_title + '</a><br>'
+
+    return HttpResponse(html)
+
+def detail(request, album_id):
+    return HttpResponse("<h2>Details for Album id : " + str(album_id) + "</h2>")
